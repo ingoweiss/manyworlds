fd696c5f0deefc5ba3998c2018c3a422357ff980
Ingo Weiss <ingo@ingoweiss.com>
2020-03-16 15:57:53 -0400

    add --strict argument to cli

--- manyworlds.py
+++ manyworlds.py
@@ -6,11 +6,12 @@ parser = argparse.ArgumentParser(prog="manyworld.py")
 parser.add_argument('action', choices=['flatten', 'graph'])
 parser.add_argument('--input')
 parser.add_argument('--output')
+parser.add_argument('--strict', action='store_true')
 args = parser.parse_args()
 
 tree = mw.ScenarioTree(args.input)
 
 if args.action == 'flatten':
-    tree.flatten(args.output)
+    tree.flatten(args.output, strict=args.strict)
 elif args.action == 'graph':
     tree.graph(args.output)
