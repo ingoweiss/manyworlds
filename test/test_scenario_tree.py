f7b46b27d929a402a34c384d1ea7a20cc53d6981
Ingo Weiss <ingo@ingoweiss.com>
2020-03-19 19:29:54 -0400

    add test/__init__.py to fix pytest

--- test/test_scenario_tree.py
+++ test/test_scenario_tree.py
@@ -3,10 +3,10 @@ import pdb
 import filecmp
 sys.path.insert(0, '..')
 
-from manyworlds import scenario_tree as st
+from manyworlds.scenario_tree import ScenarioTree
 
 def test_parse():
-    tree = st.ScenarioTree('test/fixtures/scenarios_tree.feature')
+    tree = ScenarioTree('test/fixtures/scenarios_tree.feature')
     assert len(tree.roots) == 1
     bulk_change_scenario = tree.roots[0].children[0].children[1].children[1]
     assert bulk_change_scenario.name == 'Bulk change permissions'
@@ -14,16 +14,16 @@ def test_parse():
     assert len(bulk_change_scenario.assertions) == 2
 
 def test_flatten_strict():
-    tree = st.ScenarioTree('test/fixtures/scenarios_tree.feature')
+    tree = ScenarioTree('test/fixtures/scenarios_tree.feature')
     tree.flatten('test/out/scenarios_flat_strict.feature')
     assert filecmp.cmp('test/out/scenarios_flat_strict.feature', 'test/fixtures/scenarios_flat_strict.feature')
 
 def test_flatten_relaxed():
-    tree = st.ScenarioTree('test/fixtures/scenarios_tree.feature')
+    tree = ScenarioTree('test/fixtures/scenarios_tree.feature')
     tree.flatten('test/out/scenarios_flat_relaxed.feature', strict=False)
     assert filecmp.cmp('test/out/scenarios_flat_relaxed.feature', 'test/fixtures/scenarios_flat_relaxed.feature')
 
 def test_graph():
-    tree = st.ScenarioTree('test/fixtures/scenarios_tree.feature')
+    tree = ScenarioTree('test/fixtures/scenarios_tree.feature')
     tree.graph('test/out/scenarios_graph.mermaid.txt')
     assert filecmp.cmp('test/out/scenarios_graph.mermaid.txt', 'test/fixtures/scenarios_graph.mermaid.txt')
