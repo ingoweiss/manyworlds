6896f8afef24feee582b2e9ff0e2a0aa370e6441
Ingo Weiss <ingo@ingoweiss.com>
2020-03-19 19:30:55 -0400

    rename manyworlds.py to manyworlds_cli.py

--- manyworlds_cli.py
+++ manyworlds_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from manyworlds import scenario_tree as mw
+from manyworlds.scenario_tree import ScenarioTree
 import pdb
 
 parser = argparse.ArgumentParser(prog="manyworld.py")
@@ -9,7 +9,7 @@ parser.add_argument('--output')
 parser.add_argument('--strict', action='store_true')
 args = parser.parse_args()
 
-tree = mw.ScenarioTree(args.input)
+tree = ScenarioTree(args.input)
 
 if args.action == 'flatten':
     tree.flatten(args.output, strict=args.strict)
