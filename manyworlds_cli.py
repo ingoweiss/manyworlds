05c011b98c3a7cadb904c356310a07aaee89d4ac
Ingo Weiss <ingo@ingoweiss.com>
2020-03-19 19:50:08 -0400

    improve cli arguments

--- manyworlds_cli.py
+++ manyworlds_cli.py
@@ -1,17 +1,18 @@
 import argparse
 from manyworlds.scenario_tree import ScenarioTree
-import pdb
 
 parser = argparse.ArgumentParser(prog="manyworld.py")
-parser.add_argument('action', choices=['flatten', 'graph'])
-parser.add_argument('--input')
-parser.add_argument('--output')
-parser.add_argument('--strict', action='store_true')
+parser.add_argument('action', choices=['flatten', 'graph'], help="'flatten' to output flat scenario file, 'graph' to output mermaid file")
+parser.add_argument('--input', help="input scenario file")
+parser.add_argument('--output', help="output file (either flat scenario file or mermaid file)")
+parser.add_argument('--relaxed', action='store_true', help="allow multipe 'When/Then' groups in one scenario")
 args = parser.parse_args()
 
 tree = ScenarioTree(args.input)
 
+strict = not args.relaxed
+
 if args.action == 'flatten':
-    tree.flatten(args.output, strict=args.strict)
+    tree.flatten(args.output, strict=strict)
 elif args.action == 'graph':
     tree.graph(args.output)
