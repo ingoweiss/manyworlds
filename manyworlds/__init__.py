5942ba0128470610c6a9cbf6bfd867dcba10d54d
Ingo Weiss <ingo@ingoweiss.com>
2023-08-12 14:10:13 +0200

    fix manyworlds/__init__.py linting error

--- manyworlds/__init__.py
+++ manyworlds/__init__.py
@@ -2,4 +2,5 @@ from .scenario_forest import ScenarioForest
 from .scenario import Scenario
 from .step import Step, Prerequisite, Action, Assertion
 
+__all__ = ['ScenarioForest', 'Scenario', 'Step', 'Prerequisite', 'Action', 'Assertion']
 __version__ = "0.0.2"
